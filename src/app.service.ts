3365564ac469683a3ecafd86770a1a1d5e5643a9
bhandekunal16 <bhandekunal16@gmail.com>
2024-10-30 12:11:32 +0530

    "Added port and family properties to response in AppService"

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable, Req } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(@Req() req: any) {
-    return { data: 'Hello World!', ip: req.socket.remoteAddress };
+    return {
+      data: 'Hello World!',
+      ip: req.socket.remoteAddress,
+      port: req.socket._peername.port,
+      family: req.socket._peername.family,
+    };
   }
 }
